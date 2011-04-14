6af7a4ae9b54e7f3f184f67d862d0dfea07dafbe
Jeff Li <duffercn@gmail.com>
2011-04-15 06:45:32 +0800

    Added user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,9 @@
 module SessionsHelper
 
+  def authenticate
+    deny_access unless signed_in?
+  end
+  
   def deny_access
     store_location
     redirect_to signin_path, :notice => "Please sign in to access this page."
