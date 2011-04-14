8a0dd4acd6373d67d507f0aaeed7b08ae92c8e0b
Jeff Li <duffercn@gmail.com>
2011-04-14 16:26:16 +0800

    Done with user edit/update, index, and destroy actions

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -53,10 +53,7 @@ describe "LayoutLinks" do
 
     before(:each) do
       @user = Factory(:user)
-      visit signin_path
-      fill_in :email,    :with => @user.email
-      fill_in :password, :with => @user.password
-      click_button
+      integration_sign_in(@user)
     end
 
     it "should have a signout link" do
@@ -76,10 +73,7 @@ describe "LayoutLinks" do
 
     describe "failure" do
       it "should not sign a user in" do
-        visit signin_path
-        fill_in :email,    :with => ""
-        fill_in :password, :with => ""
-        click_button
+        integration_sign_in(User.new(:email=>'',:password=>''))
         response.should have_selector("div.flash.error", :content => "Invalid")
       end
     end
@@ -87,10 +81,7 @@ describe "LayoutLinks" do
     describe "success" do
       it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in :email,    :with => user.email
-        fill_in :password, :with => user.password
-        click_button
+        integration_sign_in(user)
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not be_signed_in
