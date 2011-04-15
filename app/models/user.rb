6ca334a78082354ab3b5c3ea695f477e511aee5a
Jeff Li <duffercn@gmail.com>
2011-04-15 11:59:45 +0800

    Added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,13 @@ class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -30,14 +37,13 @@ class User < ActiveRecord::Base
                        :length       => { :within => 6..40 }    
                        
   before_save :encrypt_password
-
-  def has_password?(submitted_password)
-    encrypted_password == encrypt(submitted_password)
-  end
   
   def feed
-    # This is preliminary. See Chapter 12 for the full implementation.
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
+  end
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
   end
   
   def self.authenticate(email, submitted_password)
@@ -49,6 +55,18 @@ class User < ActiveRecord::Base
   def self.authenticate_with_salt(id, cookie_salt)
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
+  end
+
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+  
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
   end
 
   private
@@ -69,4 +87,6 @@ class User < ActiveRecord::Base
     def secure_hash(string)
       Digest::SHA2.hexdigest(string)
     end
+    
+    
 end
